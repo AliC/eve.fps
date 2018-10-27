b8956dcf1c7f198af55b8981151099eca580c0cf
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-10-27 15:18:15 +0100

    new web app from template

---
+++ src/adc.eve.fps/adc.eve.fps.ui.web/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace adc.eve.fps.ui.web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
